e73e0bd24a53ce546b3ed37f4253414ea1cdb9e8
Jan N Rose <janniklas.rose@gmail.com>
2026-05-17 16:11:23 +0200

    Use main function for test.py

--- test.py
+++ test.py
@@ -7,13 +7,18 @@ tensor = [
     [0.1, 0, 3]  # small perturbation
 ]
 
-dt = DiffusionTensor(tensor)
 
-print("Tensor properties:")
-print("full tensor\n", dt.tensor)
-print("eigenvalues\n", dt.EigenValues)
-print("eigenvectors\n", dt.EigenVectors)
-print("Diffusion Tensor properties:")
-print(f"L1 = {dt.L1}, L2 = {dt.L2}, L3 = {dt.L3}")
-print(f"E1 = {dt.E1}, E2 = {dt.E2}, E3 = {dt.E3}")
-print(f"MD = {dt.MD}, FA = {dt.FA}, Mo = {dt.Mo}")
+def main():
+    dt = DiffusionTensor(tensor)
+    print("Tensor properties:")
+    print("full tensor\n", dt.tensor)
+    print("eigenvalues\n", dt.EigenValues)
+    print("eigenvectors\n", dt.EigenVectors)
+    print("Diffusion Tensor properties:")
+    print(f"L1 = {dt.L1}, L2 = {dt.L2}, L3 = {dt.L3}")
+    print(f"E1 = {dt.E1}, E2 = {dt.E2}, E3 = {dt.E3}")
+    print(f"MD = {dt.MD}, FA = {dt.FA}, Mo = {dt.Mo}")
+
+
+if __name__ == "__main__":
+    main()
